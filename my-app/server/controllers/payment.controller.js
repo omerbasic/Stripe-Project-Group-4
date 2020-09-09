62f8a0e0827791e8d575536aa02a104fe794ea72
Ensar Omerbasic <omerbasic.ensar@gmail.com>
2020-09-09 13:01:04 +0200

    can redirect

--- my-app/server/controllers/payment.controller.js
+++ my-app/server/controllers/payment.controller.js
@@ -1,10 +1,32 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
-async function createCheckoutSession(req, res) {
+async function createCheckoutSession(req, res, body) {
+    console.log("hej")
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: [
+        
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "T-shirt",
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Shorts",
+            },
+            unit_amount: 2000,
+          },
+          quantity: 3,
+        }
         // Here we have to send clonedCart[] as body
       ],
       mode: "payment",
--- my-app/src/components/TotalPrice.tsx
+++ my-app/src/components/TotalPrice.tsx
@@ -1,9 +1,10 @@
 import React, { Component, CSSProperties} from 'react';
 import {CartConsumer, ContextState} from './context/cartContext'; 
-import CheckoutFracksatt from './checkoutFracksatt';
-import { Button } from 'antd';
-import StripeLogo from './StripeLogo'
-
+import { loadStripe } from '@stripe/stripe-js';
+import StripeLogo from './StripeLogo';
+import { Link } from 'react-router-dom';
+import ProductImage from './ProductImage';
+const stripePromise = loadStripe('pk_test_51HMTlsJD5SDAJbgRLvNPePxIqzQJmdnTBEjP3t9CiEBpP2gef5CYBC22T54K8gcPVYTxR7VERJcyphbV6C5gz9Iv00nA3PCKtj')
 
 
 export interface State {
@@ -33,15 +34,37 @@ export class TotalPrice extends Component<{}, State>{
         })
     }
     // End of the test function 
+        handleClick = async (event:any) => {
+        // Get Stripe.js instance
+        const stripe = await stripePromise;
+    
+        // Call your backend to create the Checkout Session
+        const response = await fetch("/api/checkout-session", { method: 'POST' });
+    
+        const session = await response.json();
+    
+        // When the customer clicks on the button, redirect them to Checkout.
+        const result = await stripe!.redirectToCheckout({
+          sessionId: session.id,
+        });
+    
+        if (result.error) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer
+          // using `result.error.message`.
+        }
+      };
 
         render(){
+
+            
             return(
                 <CartConsumer>
-                    {() => (
+                    {(contextData: ContextState) => (
                         <div style={CheckoutContainer}>
                             <h3>Card Payment</h3>
                             <p>Proceed with payment via card with</p>
-                            <button style={ButtonNoBorder} onClick={this.testOnclick}>
+                            <button style={ButtonNoBorder} onClick={this.handleClick}>
 
                                <StripeLogo></StripeLogo>
                             </button>
